63904f06bd2865c2c3ebb1eee9696e8f40f0ca16
Mykola Vasyl'yev <mykovasyl@gmail.com>
2021-10-13 20:53:54 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id","victory");
+
+newHeader.textContent = "Mykola is the champion";
+
+
